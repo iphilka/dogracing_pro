d651e7f6fdc16e7b1ad2fab7e0f4bebfaf481a5a
iphilka <i376765616@yandex.ru>
2024-05-04 00:51:32 +0500

    add_menu

--- dogracing_pro/static/js/project.js
+++ dogracing_pro/static/js/project.js
@@ -1,1 +1,13 @@
 /* Project specific Javascript goes here. */
+const nav = document.querySelector('.nav');
+const burger = document.querySelector('.burger')
+
+console.log(nav);
+console.log(burger);
+
+
+burger.addEventListener('click', (e)=>{
+    nav.classList.toggle('nav--active');
+    burger.classList.toggle('burger--active');
+    e.preventDefault();
+});
